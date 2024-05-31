8bfb973531e83a6a961f6b73cff36b2c34814a2b
Nour Mubarak <jmsk62@durham.ac.uk>
2024-05-31 08:57:47 +0100

    chore: Update NST.py path for content and style images

--- NST.py
+++ NST.py
@@ -1,306 +1,116 @@
-import cv2 as cv
-import numpy as np
 import torch
-from torchvision import transforms
-from torch.autograd import Variable
-from torch.optim import LBFGS
-import os
-from models.definitions.vgg19 import Vgg19
-
-IMAGENET_MEAN_255 = [123.675, 116.28, 103.53]
-IMAGENET_STD_NEUTRAL = [1, 1, 1]
+import torch.nn as nn
+from torchvision import models, transforms
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+import numpy as np
+import pandas as pd
 
 
-def load_image(img_path, target_shape="None"):
-    """
-    Load and resize the image.
-    """
-    if not os.path.exists(img_path):
-        raise Exception(f"Path not found: {img_path}")
-    img = cv.imread(img_path)[:, :, ::-1]  # convert BGR to RGB when reading
-    if target_shape is not None:
-        if isinstance(target_shape, int) and target_shape != -1:
-            current_height, current_width = img.shape[:2]
-            new_height = target_shape
-            new_width = int(current_width * (new_height / current_height))
-            img = cv.resize(img, (new_width, new_height), interpolation=cv.INTER_CUBIC)
-        else:
-            img = cv.resize(
-                img, (target_shape[1], target_shape[0]), interpolation=cv.INTER_CUBIC
-            )
-    img = img.astype(np.float32)
-    img /= 255.0
-    return img
+# Define the text encoder network
+class TextEncoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(TextEncoder, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 
-def prepare_img(img_path, target_shape, device):
-    """
-    Normalize the image.
-    """
-    img = load_image(img_path, target_shape=target_shape)
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Lambda(lambda x: x.mul(255)),
-            transforms.Normalize(mean=IMAGENET_MEAN_255, std=IMAGENET_STD_NEUTRAL),
-        ]
-    )
-    img = transform(img).to(device).unsqueeze(0)
-    return img
+# Define the generator network
+class Generator(nn.Module):
+    def __init__(self, latent_dim, output_dim):
+        super(Generator, self).__init__()
+        self.fc = nn.Linear(latent_dim, output_dim)
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        x = self.relu(self.fc(x))
+        x = self.tanh(x)
+        return x
 
 
-def save_image(img, img_path):
-    if len(img.shape) == 2:
-        img = np.stack((img,) * 3, axis=-1)
-    cv.imwrite(img_path, img[:, :, ::-1])  # convert RGB to BGR while writing
+# Define the dataset class
+class CustomDataset(Dataset):
+    def __init__(self, image_paths, text_data, transform=None):
+        self.image_paths = image_paths
+        self.text_data = text_data
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.image_paths[idx])
+        text = self.text_data[idx]
+        if self.transform:
+            img = self.transform(img)
+        return img, text
 
 
-def generate_out_img_name(config):
-    """
-    Generate a name for the output image.
-    Example: 'c1-s1.jpg'
-    where c1: content_img_name, and
-          s1: style_img_name.
-    """
-    prefix = (
-        os.path.basename(config["content_img_name"]).split(".")[0]
-        + "_"
-        + os.path.basename(config["style_img_name"]).split(".")[0]
-    )
-    suffix = f'{config["img_format"][1]}'
-    return prefix + suffix
-
-
-def save_and_maybe_display(
-    optimizing_img, dump_path, config, img_id, num_of_iterations
-):
-    """
-    Save the generated image.
-    If saving_freq == -1, only the final output image will be saved.
-    Else, intermediate images can be saved too.
-    """
-    saving_freq = -1
-    out_img = optimizing_img.squeeze(axis=0).to("cpu").detach().numpy()
-    out_img = np.moveaxis(out_img, 0, 2)
-
-    if img_id == num_of_iterations - 1:
-        img_format = config["img_format"]
-        out_img_name = (
-            str(img_id).zfill(img_format[0]) + img_format[1]
-            if saving_freq != -1
-            else generate_out_img_name(config)
-        )
-        dump_img = np.copy(out_img)
-        dump_img += np.array(IMAGENET_MEAN_255).reshape((1, 1, 3))
-        dump_img = np.clip(dump_img, 0, 255).astype("uint8")
-        cv.imwrite(os.path.join(dump_path, out_img_name), dump_img[:, :, ::-1])
-
-
-def prepare_model(device):
-    """
-    Load VGG19 model into local cache.
-    """
-    model = Vgg19(requires_grad=False, show_progress=True)
-    content_feature_maps_index = model.content_feature_maps_index
-    style_feature_maps_indices = model.style_feature_maps_indices
-    layer_names = model.layer_names
-    content_fms_index_name = (
-        content_feature_maps_index,
-        layer_names[content_feature_maps_index],
-    )
-    style_fms_indices_names = (style_feature_maps_indices, layer_names)
-    return model.to(device).eval(), content_fms_index_name, style_fms_indices_names
-
-
-def gram_matrix(x, should_normalize=True):
-    """
-    Generate gram matrices of the representations of content and style images.
-    """
-    (b, ch, h, w) = x.size()
-    features = x.view(b, ch, w * h)
-    features_t = features.transpose(1, 2)
-    gram = features.bmm(features_t)
-    if should_normalize:
-        gram /= ch * h * w
-    return gram
-
-
-def total_variation(y):
-    """
-    Calculate total variation.
-    """
-    return torch.sum(torch.abs(y[:, :, :, :-1] - y[:, :, :, 1:])) + torch.sum(
-        torch.abs(y[:, :, :-1, :] - y[:, :, 1:, :])
-    )
-
-
-def build_loss(
-    neural_net,
-    optimizing_img,
-    target_representations,
-    content_feature_maps_index,
-    style_feature_maps_indices,
-    config,
-):
-    """
-    Calculate content_loss, style_loss, and total_variation_loss.
-    """
-    target_content_representation = target_representations[0]
-    target_style_representation = target_representations[1]
-    current_set_of_feature_maps = neural_net(optimizing_img)
-    current_content_representation = current_set_of_feature_maps[
-        content_feature_maps_index
-    ].squeeze(axis=0)
-    content_loss = torch.nn.MSELoss(reduction="mean")(
-        target_content_representation, current_content_representation
-    )
-    style_loss = 0.0
-    current_style_representation = [
-        gram_matrix(x)
-        for cnt, x in enumerate(current_set_of_feature_maps)
-        if cnt in style_feature_maps_indices
+# Define data transformation
+transform = transforms.Compose(
+    [
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ]
-    for gram_gt, gram_hat in zip(
-        target_style_representation, current_style_representation
-    ):
-        style_loss += torch.nn.MSELoss(reduction="sum")(gram_gt[0], gram_hat[0])
-    style_loss /= len(target_style_representation)
-    tv_loss = total_variation(optimizing_img)
-    total_loss = (
-        config["content_weight"] * content_loss
-        + config["style_weight"] * style_loss
-        + config["tv_weight"] * tv_loss
-    )
-    return total_loss, content_loss, style_loss, tv_loss
+)
 
+# Load skin cancer image paths and textual descriptions
+image_paths = [
+    "/home/nour/Coding-projects/GANs dark skin/challenge_data/jpeg/train"
+]  # List of image file paths
+text_data = ["challenge_data/train.csv"]  # List of textual descriptions
 
-def make_tuning_step(
-    neural_net,
-    optimizer,
-    target_representations,
-    content_feature_maps_index,
-    style_feature_maps_indices,
-    config,
-):
-    """
-    Performs a step in the tuning loop.
-    (We are tuning only the pixels, not the weights.)
-    """
+# Define batch size
+batch_size = 32
+text_dim = 100  # Define the value of text_dim
+latent_dim = 256  # Define the value of latent_dim
+image_dim = 3  # Define the value of image_dim
 
-    def tuning_step(optimizing_img):
-        total_loss, content_loss, style_loss, tv_loss = build_loss(
-            neural_net,
-            optimizing_img,
-            target_representations,
-            content_feature_maps_index,
-            style_feature_maps_indices,
-            config,
-        )
-        total_loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-        return total_loss, content_loss, style_loss, tv_loss
+# Create dataset and dataloader
+dataset = CustomDataset(image_paths, text_data, transform=transform)
+lr = 0.001  # Define the learning rate
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    return tuning_step
+# Define the text encoder, generator, and discriminator
+text_encoder = TextEncoder(input_dim=text_dim, hidden_dim=256, output_dim=latent_dim)
+generator = Generator(latent_dim=latent_dim, output_dim=image_dim)
+discriminator = models.resnet18(pretrained=True)  # Example discriminator
 
+# Define optimizers
+optimizer_text_encoder = torch.optim.Adam(text_encoder.parameters(), lr=lr)
+optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr)
+optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr)
 
-def neural_style_transfer(config):
-    """
-    The main Neural Style Transfer method.
-    """
-    content_img_path = os.path.join(
-        config["content_images_dir"], config["content_img_name"]
-    )
-    style_img_path = os.path.join(config["style_images_dir"], config["style_img_name"])
-    out_dir_name = (
-        "combined_"
-        + os.path.split(content_img_path)[1].split(".")[0]
-        + "_"
-        + os.path.split(style_img_path)[1].split(".")[0]
-    )
-    dump_path = os.path.join(config["output_img_dir"], out_dir_name)
-    os.makedirs(dump_path, exist_ok=True)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    content_img = prepare_img(content_img_path, config["height"], device)
-    style_img = prepare_img(style_img_path, config["height"], device)
+# Define loss functions
+adversarial_loss = nn.BCELoss()
+image_reconstruction_loss = nn.MSELoss()
 
-    init_img = content_img
+# Training loop
+num_epochs = 10  # Define the number of epochs
+for epoch in range(num_epochs):
+    for i, (images, texts) in enumerate(dataloader):
+        # Train the text encoder
+        optimizer_text_encoder.zero_grad()
+        text_embeddings = text_encoder(texts)
 
-    optimizing_img = Variable(init_img, requires_grad=True)
-    neural_net, content_feature_maps_index_name, style_feature_maps_indices_names = (
-        prepare_model(device)
-    )
-    print(f"Using VGG19 in the optimization procedure.")
-    content_img_set_of_feature_maps = neural_net(content_img)
-    style_img_set_of_feature_maps = neural_net(style_img)
-    target_content_representation = content_img_set_of_feature_maps[
-        content_feature_maps_index_name[0]
-    ].squeeze(axis=0)
-    target_style_representation = [
-        gram_matrix(x)
-        for cnt, x in enumerate(style_img_set_of_feature_maps)
-        if cnt in style_feature_maps_indices_names[0]
-    ]
-    target_representations = [
-        target_content_representation,
-        target_style_representation,
-    ]
-    num_of_iterations = 1000
+        # Train the generator
+        optimizer_generator.zero_grad()
+        generated_images = generator(text_embeddings)
 
-    optimizer = LBFGS(
-        (optimizing_img,), max_iter=num_of_iterations, line_search_fn="strong_wolfe"
-    )
-    cnt = 0
+        # Train the discriminator
+        optimizer_discriminator.zero_grad()
+        discriminator_output_real = discriminator(images)
+        discriminator_output_fake = discriminator(generated_images.detach())
 
-    def closure():
-        nonlocal cnt
-        if torch.is_grad_enabled():
-            optimizer.zero_grad()
-        total_loss, content_loss, style_loss, tv_loss = build_loss(
-            neural_net,
-            optimizing_img,
-            target_representations,
-            content_feature_maps_index_name[0],
-            style_feature_maps_indices_names[0],
-            config,
-        )
-        if total_loss.requires_grad:
-            total_loss.backward()
-        with torch.no_grad():
-            print(
-                f'L-BFGS | iteration: {cnt:03}, total loss={total_loss.item():12.4f}, content_loss={config["content_weight"] * content_loss.item():12.4f}, style loss={config["style_weight"] * style_loss.item():12.4f}, tv loss={config["tv_weight"] * tv_loss.item():12.4f}'
-            )
-            save_and_maybe_display(
-                optimizing_img, dump_path, config, cnt, num_of_iterations
-            )
-        cnt += 1
-        return total_loss
-
-    optimizer.step(closure)
-    return dump_path
-
-
-PATH = "/home/nour/Coding-projects/GANs dark skin/challenge_data"
-CONTENT_IMAGE = "/home/nour/Coding-projects/GANs dark skin/challenge_data/jpeg/train/ISIC_0074311.jpg"
-STYLE_IMAGE = "/home/nour/Coding-projects/GANs dark skin/challenge_data/jpeg/train/ISIC_0075914.jpg"
-
-default_resource_dir = os.path.join(PATH, "data")
-content_images_dir = os.path.join(default_resource_dir, "content-images")
-style_images_dir = os.path.join(default_resource_dir, "style-images")
-output_img_dir = os.path.join(default_resource_dir, "output-images")
-img_format = (4, ".jpg")
-
-optimization_config = {
-    "content_img_name": CONTENT_IMAGE,
-    "style_img_name": STYLE_IMAGE,
-    "height": 400,
-    "content_weight": 100000.0,
-    "style_weight": 30000.0,
-    "tv_weight": 1.0,
-}
-optimization_config["content_images_dir"] = content_images_dir
-optimization_config["style_images_dir"] = style_images_dir
-optimization_config["output_img_dir"] = output_img_dir
-optimization_config["img_format"] = img_format
-
-results_path = neural_style_transfer(optimization_config)
+        # Update the models' parameters
+        optimizer_text_encoder.step()
+        optimizer_generator.step()
+        optimizer_discriminator.step()
